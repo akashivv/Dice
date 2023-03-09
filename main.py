340a798182164019f3a6e0d6aaff3b1bfe0dbe0a
Akashivv <116174059+akashivv@users.noreply.github.com>
2023-03-09 13:31:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+class Game:
+  def __dice__(self):
+    self.sides = input("enter the number of sides wanted on the dice ")
+    self.roll = input("How many times do you want to roll your dice")
+    
