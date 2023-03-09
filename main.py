b29d64d05c8bc4479eac1ebe9c5148983d375287
Akashivv <116174059+akashivv@users.noreply.github.com>
2023-03-09 13:33:19 +0000

    Dice Function

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ class Game:
   def __dice__(self):
     self.sides = input("enter the number of sides wanted on the dice ")
     self.roll = input("How many times do you want to roll your dice")
-    
+
+
+  def __user__(self): 
+      self.users = ("How many users are there")
+
+
+  def __diceroll__()
